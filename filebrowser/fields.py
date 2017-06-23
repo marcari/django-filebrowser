013d34a44198b570d2a4aa2ecd82615d220e562e
Marc Ariberti <marc@ariberti.net>
2017-06-23 09:00:35 +0200

    fix django 1.11

--- filebrowser/fields.py
+++ filebrowser/fields.py
@@ -72,6 +72,8 @@ class FileBrowseFormField(forms.CharField):
 
     def clean(self, value):
         value = super(FileBrowseFormField, self).clean(value)
+        if value == None:
+            value = ''
         if value == '':
             return value
         file_extension = os.path.splitext(value)[1].lower()
